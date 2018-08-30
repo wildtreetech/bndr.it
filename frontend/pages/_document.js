7737fa1cef4152d56196d6515a492946cc030cec
Tim Head <betatim@gmail.com>
2018-08-30 23:28:02 +0200

    Dangerously inject GA

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -10,6 +10,11 @@ export default class Document extends Document_ {
     return props
   }
 
+  createGA () {
+    return {
+      __html: "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n ga('create', 'UA-75797246-3', 'auto', {'storage': 'none'});\n ga('set', 'anonymizeIp', true);\n ga('send', 'pageview');"};
+  }
+
   render () {
     return (
       <html>
@@ -20,15 +25,7 @@ export default class Document extends Document_ {
             dangerouslySetInnerHTML={{ __html: '__ENV__ = ' + htmlescape(env) }}
           />
           <NextScript />
-          <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-75797246-3', 'auto', {'storage': 'none'});
-            ga('set', 'anonymizeIp', true);
-            ga('send', 'pageview');
-          </script>
+          <script dangerouslySetInnerHTML={this.createGA()} />
         </body>
       </html>
     )
